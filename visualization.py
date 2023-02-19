5a593cf1f6264d05e6d9078779fc4949cecd9d96
victorleo92 <victor@kamerapengintai.com>
2023-02-19 08:13:07 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from PIL import Image
 
+
+def main():
 # Set the title of the app
 st.title("My Streamlit App")
 
 # Show some text
 st.write("Welcome to my app!")
 
-# Load and display an image
-image = Image.open("image.jpg")
-st.image(image, caption="A beautiful image", use_column_width=True)
+if __name__ == "__main__":
+    main()
