9feb4b1e71cccad1b5956e0632883faa0bb0e12b
victorleo92 <victor@kamerapengintai.com>
2023-02-19 08:11:11 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,33 +1,12 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
+from PIL import Image
 
-# Define a function to create a chart
-def create_chart():
-    # Generate some sample data
-    x = np.linspace(0, 10, 100)
-    y = np.sin(x)
+# Set the title of the app
+st.title("My Streamlit App")
 
-    # Create a line plot of the data
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_title('Business Visualization')
+# Show some text
+st.write("Welcome to my app!")
 
-    # Return the chart as a PNG image
-    return fig
-
-# Define the Streamlit app
-def main():
-    # Set the title and subtitle of the app
-    st.title("Business Visualization App")
-    st.subheader("Using Streamlit and Matplotlib")
-
-    # Create a chart and display it
-    chart = create_chart()
-    st.pyplot(chart)
-
-# Run the Streamlit app
-if __name__ == '__main__':
-    main()
+# Load and display an image
+image = Image.open("image.jpg")
+st.image(image, caption="A beautiful image", use_column_width=True)
