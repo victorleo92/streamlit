b9cfb3c4dbac5eafd33ada106f95c881b3c33a39
victorleo92 <victor@kamerapengintai.com>
2023-02-19 08:13:59 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -2,11 +2,8 @@ import streamlit as st
 
 
 def main():
-# Set the title of the app
-st.title("My Streamlit App")
-
-# Show some text
-st.write("Welcome to my app!")
+    st.title("My Streamlit App")
+    st.write("Welcome to my app!")
 
 if __name__ == "__main__":
     main()
